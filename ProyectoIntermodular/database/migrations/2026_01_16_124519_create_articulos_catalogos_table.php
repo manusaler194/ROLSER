8bfbc03449ae16117e197c599c7ee388ef3eaf89
Ruben <rubensanan@gmail.com>
2026-01-16 13:49:19 +0100

    Tablas completas

---
+++ ProyectoIntermodular/database/migrations/2026_01_16_124519_create_articulos_catalogos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articulos_catalogos', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('id_catalogo')->nullable()->constrained('catalogos', 'id_catalogo')->nullOnDelete()->cascadeOnUpdate();
+            $table->foreignId('id_articulo')->nullable()->constrained('articulos', 'id_articulo')->nullOnDelete()->cascadeOnUpdate();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articulos_catalogos');
+    }
+};
