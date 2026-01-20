910edea855646a0127e5dda330e532c1144c0d41
Farinos1 <alexfr@faramos.es>
2026-01-20 13:41:59 +0100

    Proveedor hecho

--- ProyectoIntermodular/app/Models/Proveedor.php
+++ ProyectoIntermodular/app/Models/Proveedor.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Proveedor extends Model
 {
-    
+    // Nombre tabla en BD
+    protected $table = 'proveedores';
+
+    // Todos los atributos escepto (id, created_at, updated_at y foreign keys)
+    protected $fillable = ['nombre_empresa', 'contacto', 'cif'];
+
+    public function encargados_de_almacen(){
+        return $this->belongsToMany(EncargadoAlmacen::class);
+    }
 }
