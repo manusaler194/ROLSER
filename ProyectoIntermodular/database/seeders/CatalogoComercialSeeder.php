b00eed2f52ad39c8d9586e2d85dee6419eed7d9c
Ruben <rubensanan@gmail.com>
2026-01-28 13:13:05 +0100

    Seeder

--- ProyectoIntermodular/database/seeders/CatalogoComercialSeeder.php
+++ ProyectoIntermodular/database/seeders/CatalogoComercialSeeder.php
@@ -4,28 +4,23 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
-
-class AlmacenSeeder extends Seeder{
+class CatalogoComercialSeeder extends Seeder
+{
     /**
      * Run the database seeds.
      */
     public function run(): void{
-
         $faker = Faker::create('es_ES');
 
         for ($i = 0; $i < 10; $i++) {
-            DB::table('almacenes')->insert([
-           'direccion' => $faker->name(),
-           'capacidad' => rand(100,1000),
+            DB::table('comerciales_catalogos')->insert([
            'created_at' => date('Y-m-d'),
            'updated_at' => date('Y-m-d'),
-           "id_encargado" => rand(1,10)]);
+           "id_comercial" => rand(1,10),
+           "id_catalogo" => rand(1,10)]);
         }
     }
 }
-
