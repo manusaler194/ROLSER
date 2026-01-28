657ba43f4d6ccff6ddbc4fd59a6539feb535e6a2
Ruben <rubensanan@gmail.com>
2026-01-28 08:42:06 +0100

    Proyecto React

---
+++ React/src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true
+  }
+]
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+// axios.get('http://localhost:3009/api/notes')
+//   .then(response => {
+//     const notes = response.data
+//     ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+//   })
+//   .catch( (e) => console.log(e));
+
