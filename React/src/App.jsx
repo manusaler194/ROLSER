ec19bc5dfbf6c8fcb93cdc32f304279dac76e2b2
Ruben <rubensanan@gmail.com>
2026-01-30 10:00:41 +0100

    Navbar react

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,4 +1,15 @@
+import Header from "./components/Header"
+import Navbar from "./components/Navbar"
+
 const App = () => {
  
+    return(
+        <div>
+            <Header></Header>
+
+            <Navbar usuario = "admin"></Navbar>
+        </div>
+        
+    )
 }
 export default App
---
+++ React/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import logo from '../assets/Header/logo.jpg'
+import signout from '../assets/Header/SignOut.svg'
+const Header = () =>{
+return(
+<div className="flex items-center gap-2.5 h-50 w-full bg-[#454545]">
+
+    <img src={logo} alt="Logo" height={50} width={50} className="m-2.5 rounded-[60%] h-17 w-17"/>
+    
+    <h1 className="text-white text-3xl">ROLSER</h1>
+    <a href="" className="ml-auto mr-7.5">
+        <img src={signout} alt="Logout" className="cursor-pointer h-15 w-15"/>
+    </a>
+ </div>
+)
+}
+export default Header;
