a36770a27a89982cf214f5ac64c3ea71601c0d60
Ruben <rubensanan@gmail.com>
2026-01-30 11:21:51 +0100

    COntrolador Almacen

--- ProyectoIntermodular/app/Http/Controllers/AlmacenController.php
+++ ProyectoIntermodular/app/Http/Controllers/AlmacenController.php
@@ -3,30 +3,19 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\Almacen;
 
 class AlmacenController extends Controller{
 
-    public function mostrar(){
-        $datos = DB::table('almacenes')->get();
-
-        foreach($datos as $dato){
-            echo $dato->direccion . "<br>";
-            echo $dato->capacidad . "<br>";
-
-        }
-
-        $valor = 'Calle Falsa 123';
-        $direccion = DB::table('almacenes')->where('direccion', $valor)->get();
-        dump($direccion->toArray());
-
-        $capacidad = 50;
-        $almacenCap = DB::table('almacenes')->where('capacidad', '>', $capacidad)->get();
-        dump($almacenCap->toArray());
-
-
-        $encargado = 1;
-        $almacenEncargado = DB::table('almacenes')->where('id_encargado', $encargado)->get();
-        dump($almacenEncargado->toArray());
-
+    public function mostrar(Request $request){
+        $datos = Almacen::all();
+        return $datos;
+    }
+    public function guardar(Request $request){
+        $validatedData = $request->validate([
+            'direccion' => 'required|string|max:255',
+            'capacidad' => 'nullable|string',
+            'id_encargado' => 'nullable|string',
+        ]);
     }
 }
--- ProyectoIntermodular/routes/api.php
+++ ProyectoIntermodular/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlmacenController;
+use App\Models\Almacen;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/almacenes', [AlmacenController::class, 'mostrar']);
