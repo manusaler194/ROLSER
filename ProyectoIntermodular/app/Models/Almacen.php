4706a0568277c14e907abc2a14b0c38bd8b85574
Farinos1 <alexfr@faramos.es>
2026-02-03 12:56:54 +0100

    Conflictos arreglados

--- ProyectoIntermodular/app/Models/Almacen.php
+++ ProyectoIntermodular/app/Models/Almacen.php
@@ -4,21 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Almacen extends Model
-{
+class Almacen extends Model{
     protected $table = 'almacenes';
     protected $primaryKey = 'id_almacen';
-<<<<<<< HEAD
+
 
     protected $fillable = ['direccion', 'capacidad', 'id_encargado'];
-=======
-    protected $fillable = ['direccion', 'capacidad'];
->>>>>>> alejandro-farinos
 
-public function secciones() {
-    return $this->hasMany(Seccion::class);
-}
-public function encargadoAlmacen() {
-    return $this->belongsToMany(EncargadoAlmacen::class);
-}
+    public function secciones() {
+        return $this->hasMany(Seccion::class);
+    }
+    public function encargadoAlmacen() {
+        return $this->belongsToMany(EncargadoAlmacen::class);
+    }
 }
--- ProyectoIntermodular/routes/api.php
+++ ProyectoIntermodular/routes/api.php
@@ -4,31 +4,25 @@ use App\Http\Controllers\AdministradorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AlmacenController;
-<<<<<<< HEAD
 use App\Http\Controllers\PedidoController;
-=======
 use App\Http\Controllers\ArticuloController;
 use App\Http\Controllers\CatalogoController;
 use App\Http\Controllers\EncargadoAlmacenController;
 use App\Http\Controllers\ProveedorController;
->>>>>>> alejandro-farinos
+
 use App\Models\Almacen;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-<<<<<<< HEAD
 
-=======
->>>>>>> alejandro-farinos
 Route::get('/almacenes', [AlmacenController::class, 'mostrar']);
 Route::post('/almacenes/guardar', [AlmacenController::class, 'guardar']);
 Route::put('/almacenes/actualizar/{id_almacen}', [AlmacenController::class, 'actualizar']);
 Route::delete('/almacenes/borrar/{id_almacen}', [AlmacenController::class, 'eliminar']);
 
-<<<<<<< HEAD
-Route::get('/almacenes', [AlmacenController::class, 'mostrar']);
+
 Route::get('/administradores', [AdministradorController::class, 'index']);
 Route::get('/users', [AdministradorController::class, 'userIndex']);
 
@@ -37,7 +31,7 @@ Route::post('/pedidos/guardar', [PedidoController::class, 'guardar']);
 Route::get('/pedidos', [PedidoController::class, 'mostrar']);
 Route::put('/pedidos/actualizar/{id_pedido}', [PedidoController::class, 'actualizar']);
 Route::delete('/pedidos/borrar/{id_pedido}', [PedidoController::class, 'eliminar']);
-=======
+
 
 Route::get('/proveedor', [ProveedorController::class, 'mostrarProveedores']);
 Route::post('/proveedor/guardar', [ProveedorController::class, 'proveedorNuevo']);
@@ -59,4 +53,4 @@ Route::get('/encargadoAlmacen', [EncargadoAlmacenController::class, 'mostrarEnca
 Route::post('/encargadoAlmacen/guardar', [EncargadoAlmacenController::class, 'encargadoAlmacenNuevo']);
 Route::put('/encargadoAlmacen/actualizar/{id_encargado}', [EncargadoAlmacenController::class, 'encargadoAlmacenActualizar']);
 Route::delete('/encargadoAlmacen/borrar/{id_encargado}', [EncargadoAlmacenController::class, 'eliminarEncargadoAlmacen']);
->>>>>>> alejandro-farinos
+
