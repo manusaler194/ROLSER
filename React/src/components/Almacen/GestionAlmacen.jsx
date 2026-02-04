b56c12c89a72c5705e2d58e8a3a61ecfa46122ea
Ruben <rubensanan@gmail.com>
2026-02-04 09:46:21 +0100

    Vista crear almacén

--- React/src/components/Almacen/GestionAlmacen.jsx
+++ React/src/components/Almacen/GestionAlmacen.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-// Importamos el icono
 import iconoDesplegable from '/src/assets/desplegable.svg';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
 const GestionAlmacen = () => {
   const [abrirMenu, setAbrirMenu] = useState(null);
@@ -23,13 +22,11 @@ const GestionAlmacen = () => {
     <div className="p-10 flex flex-col h-full">
       <div className="flex flex-col gap-6 w-72">
         {almacenes.map((almacen) => (
-          <div key={almacen.id_almacen} className="relative flex items-center">
-
+          <div key={almacen.id_almacen} className="relative flex items-center w-100 h-20">
             <button 
               onClick={() => setAbrirMenu(abrirMenu === almacen.id_almacen ? null : almacen.id_almacen)}
               className="flex items-center justify-between w-full bg-[#757575] text-white px-5 py-3 rounded-2xl hover:bg-gray-600 cursor-pointer">
               <span className="text-xl font-medium">{almacen.direccion}</span>
-              
               <img src={iconoDesplegable} className={`w-6 h-6 transition-transform duration-500  invert ${abrirMenu === almacen.id_almacen ? 'rotate-180' : 'rotate-0'}`}/>
             </button>
             
